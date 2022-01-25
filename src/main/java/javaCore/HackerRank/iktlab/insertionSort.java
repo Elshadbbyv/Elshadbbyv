d1bcabc114fa4bb09dec03fd1443ee086846a1e9
elshadbbyv <babayevelshad09@gmail.com>
2022-01-25 15:39:16 +0400

    insertionSort for IKTLAB

---
+++ src/main/java/javaCore/HackerRank/iktlab/insertionSort.java
@@ -0,0 +1,34 @@
+package javaCore.HackerRank.iktlab;
+
+public class insertionSort {
+    void selectArray(int[] array) {
+        int a = array.length;
+        for (int i = 1; i < a; i++) {
+            int b = array[i];
+            int k = i -1;
+            while(k>=0 && array[k]>b){
+                array[k+1]=array[k];
+                k = k-1;
+
+
+            }
+            array[k+1]=b;
+
+
+
+        }
+    }
+    static void printArray(int[] array){
+        int a = array.length;
+        for (int i = 0; i < a; ++i) {
+            System.out.print(array[i]+" ");
+        }
+        System.out.println();
+    }
+    public static void main(String[] args) {
+        int[] array = {76,34,67,12,89,45,23};
+        insertionSort sorting = new insertionSort();
+        sorting.selectArray(array);
+        printArray(array);
+    }
+}
